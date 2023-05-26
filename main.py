653b7604c87021664d9246317bee4a38506e9d04
Charan Teja Kattumenu <charanteja.kattumenu@gmail.com>
2023-05-26 20:27:06 +0530

    Update prompts

--- main.py
+++ main.py
@@ -1,66 +1,40 @@
 from langchain.llms import OpenAI
-from langchain.chains import LLMChain, TransformChain, SimpleSequentialChain
 from langchain.agents import initialize_agent
 from langchain.agents.agent_toolkits import ZapierToolkit
 from langchain.agents import AgentType
-from langchain.agents import load_tools
 from langchain.utilities.zapier import ZapierNLAWrapper
-from langchain.tools.zapier.tool import ZapierNLARunAction
 from langchain.prompts import PromptTemplate
 
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-llm = OpenAI(temperature=0)
+llm = OpenAI(model_name="text-davinci-003", temperature=1)
 zapier = ZapierNLAWrapper()
-# toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
-# tools = load_tools(["serpapi", "llm-math"], llm=llm)
-# agent = initialize_agent(toolkit.get_tools() + tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
+tools = toolkit.get_tools()
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
-# template = """You are an AI assisstant who schedules, accepts, rejects meetings on google calendar.
-# Get details of my google calendar account from zapier.
-# Use google search engine wherever you deem necessary.
-# {user}
-# """
+template = """
+    You are helpful assistant that schedules, modifies, creates and views google calendar meetings.
 
-# prompt_template = PromptTemplate.from_template(template=template)
+    Your goal is to help the user manage the busy scheduling meetings in a way that optimizes user's time.
 
-# agent.run(prompt_template.format(user="Schedule a meeting for me tomorrow for 30mins between 2pm-5pm IST with shaan@biconomy.io"))
+    Try to be precise about the information you share with user in the final step.
 
-## step 0
-actions = ZapierNLAWrapper().list()
+    This is the user input: {user_input}
+"""
 
-# ## step 1. find events in calendar
+prompt_template = PromptTemplate.from_template(template)
 
-GOOGLE_CALENDAR_INSTRUCTIONS = "Get the list of events scheduled on google calendar for this week for charan@biconomy.io"
+# Prompt 1 
+# output = agent.run(prompt_template.format(user_input="Schedulie a 30 minute meeting including me and Shaan(shaan.sundar@biconomy.io) tomorrow at 12pm IST with title 'DYOR' on my google calendar. Share meeting details(title,meeting link, time) with me here"))
+# print(output)
 
-def nla_gcalendar(inputs):
-    action = next((a for a in actions if a["description"].startswith("Google Calendar: Find Event")), None)
-    return {"calendar_data": ZapierNLARunAction(action_id=action["id"], zapier_description=action["description"], params_schema=action["params"]).run(inputs["instructions"])}
-calendar_chain = TransformChain(input_variables=["instructions"], output_variables=["calendar_data"], transform=nla_gcalendar)
+# Prompt 2
+output = agent.run(prompt_template.format(user_input="update my slack status to 'Working Remotely'"))
+print(output)
 
-# ## step 2. generate draft reply
-
-# template = """You are an assisstant who schedules, accepts, rejects meetings on google calendar. Identify status of scheduled meeting and write it (name, meeting link, status) in plain text (not JSON).
-
-# Incoming calendar event:
-# {calendar_data}
-
-# Event Status:"""
-
-# prompt_template = PromptTemplate(input_variables=["calendar_data"], template=template)
-# status_chain = LLMChain(llm=OpenAI(temperature=.7), prompt=prompt_template)
-
-# ## step 3. send draft reply via a slack direct message
-
-# SLACK_HANDLE = "@Charan"
-
-# def nla_slack(inputs):
-#     action = next((a for a in actions if a["description"].startswith("Slack: Send Direct Message")), None)
-#     instructions = f'Send this to {SLACK_HANDLE} in Slack: {inputs["draft_reply"]}'
-#     return {"slack_data": ZapierNLARunAction(action_id=action["id"], zapier_description=action["description"], params_schema=action["params"]).run(instructions)}
-# slack_chain = TransformChain(input_variables=["draft_reply"], output_variables=["slack_data"], transform=nla_slack)
-
-overall_chain = SimpleSequentialChain(chains=[calendar_chain], verbose=True)
-overall_chain.run(GOOGLE_CALENDAR_INSTRUCTIONS)
+# Prompt 3
+# output = agent.run(prompt_template.format(user_input="Block my calendar for 3 hours from now with status 'busy'."))
+# print(output)
